tcdude/foolysh

commit 4cb65b17ecbbac9b470d208ab4a7a3d91ed85621

Improve forget and update docstrings
Previously forget would leave empty dicts when forgetting an
event.
Docstrings updated to new style.

foolysh/eventhandler.py
@@ -40,15 +40,15 @@ class EventHandler(object):
         """
         Adds a callback to be executed at every call of the EventHandler.
 
-        :param name: Unique name of the event
-        :param sdl_event: SDL event type tested against
-            ``sdl2.ext.get_events()``
-        :param callback: Method to execute. Must provide ``event`` as named
-            argument.
-        :param priority: Call priority, higher numbers get executed first
-            (default=0).
-        :param args: optional positional arguments to pass to ``callback``.
-        :param kwargs: optional keyword arguments to pass to ``callback``.
+        Args:
+            name: Unique name of the event
+            sdl_event: SDL event type tested against ``sdl2.ext.get_events()``
+            callback: Method to execute. Must provide ``event`` as named
+                argument.
+            priority: Call priority, higher numbers get executed first
+                (default=0).
+            args: optional positional arguments to pass to ``callback``.
+            kwargs: optional keyword arguments to pass to ``callback``.
         """
         if name in self._unique:
             raise ValueError('An event with this name already exists.')
@@ -57,14 +57,24 @@ class EventHandler(object):
         self._unique[name] = (callback, args, kwargs)
 
     def forget(self, name):
-        """Removes event ``name`` from the EventHandler"""
+        """
+        Removes event from the EventHandler.
+
+        Args:
+            name: ``str`` unique name of the event to be removed.
+        """
         for e in self._events:
             if name in self._events[e]:
                 self._events[e].pop(name)
+            if not self._events[e]:
+                self._events.pop(e)
         if name in self._unique:
             self._unique.pop(name)
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *unused_args, **unused_kwargs):
+        """
+        Check and execute events.
+        """
         for event in sdl2.ext.get_events():
             if event.type in self._events:
                 k = reversed(sorted(

commit 478f654dc5c17a3042793d704065dcf2bc3b5ba3

Fix: forget would raise an exception
Regression, introduced in 4cb65b1 that lead to modifying a dict
inside a for loop.

foolysh/eventhandler.py
@@ -64,11 +64,14 @@ class EventHandler(object):
         Args:
             name: ``str`` unique name of the event to be removed.
         """
+        pop_list = []
         for e in self._events:
             if name in self._events[e]:
                 self._events[e].pop(name)
             if not self._events[e]:
-                self._events.pop(e)
+                pop_list.append(e)
+        for e in pop_list:
+            self._events.pop(e)
         if name in self._unique:
             self._unique.pop(name)
 