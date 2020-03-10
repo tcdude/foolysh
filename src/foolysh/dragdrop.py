tcdude/foolysh

commit 879c0d964411306af1940ccf6eb853c025f72438

Add hidden property to Node
 * Needed to determine whether UIHandler needs to consider a Node.
 * Needed to determine whether DragDrop needs to consider a Node.

src/foolysh/dragdrop.py
@@ -164,6 +164,8 @@ class DragDrop:
             d_max = None
             for k in self._drag_nodes:
                 d_node = self._drag_nodes[k]
+                if d_node.hidden:
+                    continue
                 if d_node.node.aabb.overlap(mouse_aabb):
                     d_depth = d_node.node.relative_depth
                     if click_node is None or d_depth > d_max:

ext/src/node.cpp
@@ -284,6 +284,26 @@ query(AABB& aabb, const bool depth_sorted) {
 }
 
 /**
+ * Whether the Node is currently hidden in the scene.
+ **/
+bool Node::
+hidden() {
+    if (sgdh.flag_vec[node_id] & HIDDEN) {
+        return true;
+    }
+    size_t nid = node_id;
+    size_t parent = sgdh.parent_vec[node_id];
+    while (parent != nid) {
+        if (sgdh.flag_vec[nid] & HIDDEN) {
+            return true;
+        }
+        nid = parent;
+        parent = sgdh.parent_vec[nid];
+    }
+    return false;
+}
+
+/**
  * Hide the Node.
  **/
 void Node::

ext/src/node.hpp
@@ -182,6 +182,7 @@ public:
     void reparent_to(const size_t parent);
     bool traverse(const bool local = false);
     SmallList<size_t> query(AABB& aabb, const bool depth_sorted = true);
+    bool hidden();
     void hide();
     void show();
     void propagate_dirty();

src/foolysh/scene/cppnode.pxd
@@ -106,6 +106,7 @@ cdef extern from "src/node.hpp" namespace "foolysh::scene":
         void reparent_to(const size_t) except +
         bint traverse(bint local = False) except +
         SmallList[size_t] query(AABB&, const bint) except +
+        bint hidden()
         void hide()
         void show()
         void propagate_dirty()

src/foolysh/scene/node.pyx
@@ -242,6 +242,11 @@ cdef class Node:
         deref(self.thisptr).show()
 
     @property
+    def hidden(self):
+        """Whether the Node is currently hidden in the scene."""
+        return deref(self.thisptr).hidden()
+
+    @property
     def pos(self):
         """
         :class:`foolysh.tools.vec2.Vec2` position of this Node, relative

src/foolysh/ui/uihandler.py
@@ -192,6 +192,8 @@ class UIHandler:
                                 key=lambda x: self._nodes[x[0]].relative_depth,
                                 reverse=True)
         for node_id, event_t in sort_callbacks:
+            if self._nodes[node_id].hidden:
+                continue
             nd_bb = self._nodes[node_id].aabb
             exc = False
             if click and event_t in (EventType.CLICK, EventType.INPUT):

commit e6d3e0473ae8f1028fbc0c144268d748ee08843c

Fix bug introduced in 879c0d9

src/foolysh/dragdrop.py
@@ -164,7 +164,7 @@ class DragDrop:
             d_max = None
             for k in self._drag_nodes:
                 d_node = self._drag_nodes[k]
-                if d_node.hidden:
+                if d_node.node.hidden:
                     continue
                 if d_node.node.aabb.overlap(mouse_aabb):
                     d_depth = d_node.node.relative_depth